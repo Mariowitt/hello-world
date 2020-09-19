d958ad1ec66b213304019866d233e23ad6fe33e4
PimmelDeluxe <71567039+PimmelDeluxe@users.noreply.github.com>
2020-09-19 23:00:45 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+
+
+#das ist ein python Test der anschließen auf github hochgeladen wird!!
+
+for i in range(1,10): 
+
+	print("Hello das ist ein test:")
+
+
+print("geben sie eine Zahl ein")
+
+zahl = input()
+
+print("Ihre Eingabe war " + str(zahl))
